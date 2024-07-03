9970cbf93138aaebf0e2234ec31d811327bcd681
Victor <ban.victor728@gmail.com>
2024-07-03 14:52:06 -0400

    fixed up html page. linked it to JS. wrote the JS functionality & checked it.

--- index.js
+++ index.js
@@ -25,6 +25,28 @@
 
 */
 
+// take user input
+const userOrder = prompt("Please enter your froyo order by flavor name. Please separate with commas and do not include spaces", "Chocolate,Chocolate,Vanilla,Strawberry,Chocolate" );
 
 
+// turn string input into an array that can be sorted
+const userOrderArray = userOrder.split(`,`);
+
+
+// create an empty object to take the (array)order
+const froyoOrderObject = {};
+
+for (let i = 0; i < userOrderArray.length; i++){
+  let flavorName = userOrderArray[i];
+
+  // if the flavor(key) exists, add 1 to the value; otherwise add the key and set the value to 1
+  if (froyoOrderObject[flavorName]){
+    froyoOrderObject[flavorName]++;
+  } else
+  froyoOrderObject[flavorName] = 1;
+}
+
+console.log(froyoOrderObject)
+
+// additional thought is I would like to sanitize the user input. would like to ask about that in class & best/common practices
 
