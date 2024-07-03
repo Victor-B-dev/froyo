f8425614f07fab29f1cc39d906febae900f41b7a
Victor <ban.victor728@gmail.com>
2024-07-03 14:02:14 -0400

    initial commit. wrote psuedo code

---
+++ index.js
@@ -0,0 +1,30 @@
+/* Psuedo Code for Initial Commit
+
+  Take user input and store as a variable
+  Take variable, should be a string, and split() it into an array of strings
+  Loop through the array of strings and add them to an object (need to create an object)
+  
+  rules for the object
+  need to keep count of each flavor
+
+  let froyoOrder = {
+    flavorname : count#,
+    flavorname2 : count#,
+    flavorname3 : count#,
+  }
+
+  need to write a rule for the loop that if something exists in the order already, add to the count
+  otherwise add flavorName and set count to 1 (related to updating values in keyvalues pairs)
+
+  will be using either
+  Object.keys(userInputtedFroyoOrder)
+  For (let froyoFlavor in userInputtedFroyoOrder)
+
+  Return Object with froyo flavor numbers
+
+
+*/
+
+
+
+
